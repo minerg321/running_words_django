8ca0e6a47d4f4fb5d425be01e157274475d6587c
Mihail Lavruhin <36154473+minerg321@users.noreply.github.com>
2023-06-29 11:17:22 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),  # home 
     path('make_video/<str:text>/', views.MyView.as_view(), name='make_video'),
 ]
