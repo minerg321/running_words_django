94ff2eb56b4e84939d12f2f0bc4b834c6a0c9cda
Mihail Lavruhin <36154473+minerg321@users.noreply.github.com>
2023-06-29 11:07:01 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-1xnjdza^-^%rnq@pt)_e3ift@=65=fnhnfga(usruert8joq%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['.ngrok.io', 'localhost', '127.0.0.1']
 
 # Application definition
 
