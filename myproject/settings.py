8c6a0032042e0d299e438ea86cb2ab1ccc9d2bb4
Mihail Lavruhin <36154473+minerg321@users.noreply.github.com>
2023-06-29 10:40:46 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+print('This is the settings file that is being used')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
